61a35ce3eab98a19f7161754dcd14b7903be63ee
Matthew Odle <matt.odle@tdstelecom.com>
2017-08-04 23:25:04 -0500

    fix indents 4 > 2

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,25 +3,25 @@ var ismData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
-    // Populate the ism table on initial page load
-    populateTable();
+  // Populate the ism table on initial page load
+  populateTable();
 
-    hideAddOrUpdateForm();
+  hideAddOrUpdateForm();
 
-    // Add or Update Ism button click
-    $('#btnAddOrUpdateIsm').on('click', addOrUpdateIsm);
+  // Add or Update Ism button click
+  $('#btnAddOrUpdateIsm').on('click', addOrUpdateIsm);
 
-    // New Ism button click
-    $('#newIsm').on('click', setNewIsmFormElementText);
+  // New Ism button click
+  $('#newIsm').on('click', setNewIsmFormElementText);
 
-    // Clear Ism button click
-    $('#btnClearIsm').on('click', clearIsm);
+  // Clear Ism button click
+  $('#btnClearIsm').on('click', clearIsm);
 
-    // Update Ism link click
-    $('#ismList table tbody').on('click', 'td a.linkupdateism', populateIsmFields);
+  // Update Ism link click
+  $('#ismList table tbody').on('click', 'td a.linkupdateism', populateIsmFields);
 
-    // Delete Ism link click
-    $('#ismList table tbody').on('click', 'td a.linkdeleteism', deleteIsm);
+  // Delete Ism link click
+  $('#ismList table tbody').on('click', 'td a.linkdeleteism', deleteIsm);
 });
 
 // Functions =============================================================
@@ -60,153 +60,153 @@ function clearTheFields() {
 
 // Fill table with data
 function populateTable() {
-    // Empty content string
-    console.log('entering populateTable');
+  // Empty content string
+  console.log('entering populateTable');
 
-    var tableContent = '';
-    // jQuery AJAX call for JSON
-    $.getJSON( '/isms/ismlist', function( data ) {
-        ismListData = data;
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td>' + this.source + '</td>';
-            tableContent += '<td>' + this.number + '</td>';
-            tableContent += '<td>' + (this.tags || '') + '</td>';
-            tableContent += '<td>' + this.quote + '</td>';
-            tableContent += '<td>' + this.comments + '</td>';
-            tableContent += '<td><a href="#" class="linkupdateism" rel="' + this._id + '">u</a></td>';
-            tableContent += '<td><a href="#" class="linkdeleteism" rel="' + this._id + '">d</a></td>';
-            tableContent += '</tr>';
-        });
-
-        // Inject the whole content string into our existing HTML table
-        $('#ismList table tbody').html(tableContent);
+  var tableContent = '';
+  // jQuery AJAX call for JSON
+  $.getJSON( '/isms/ismlist', function( data ) {
+    ismListData = data;
+    // For each item in our JSON, add a table row and cells to the content string
+    $.each(data, function(){
+      tableContent += '<tr>';
+      tableContent += '<td>' + this.source + '</td>';
+      tableContent += '<td>' + this.number + '</td>';
+      tableContent += '<td>' + (this.tags || '') + '</td>';
+      tableContent += '<td>' + this.quote + '</td>';
+      tableContent += '<td>' + this.comments + '</td>';
+      tableContent += '<td><a href="#" class="linkupdateism" rel="' + this._id + '">u</a></td>';
+      tableContent += '<td><a href="#" class="linkdeleteism" rel="' + this._id + '">d</a></td>';
+      tableContent += '</tr>';
     });
-    console.log('exiting populateTable');
+
+    // Inject the whole content string into our existing HTML table
+    $('#ismList table tbody').html(tableContent);
+  });
+  console.log('exiting populateTable');
 };
 
 // Add or Update Ism
 function addOrUpdateIsm(event) {
-    event.preventDefault();
-    console.log('update or add ism clicked!');
+  event.preventDefault();
+  console.log('update or add ism clicked!');
 
-    // Super basic validation - increase errorCount variable if any fields are blank
-    var errorCount = 0;
-    $('#addOrUpdateIsm input').each(function(index, val) {
-        if($(this).val() === '') { errorCount++; }
+  // Super basic validation - increase errorCount variable if any fields are blank
+  var errorCount = 0;
+  $('#addOrUpdateIsm input').each(function(index, val) {
+    if($(this).val() === '') { errorCount++; }
+  });
+
+  if(errorCount === 0) {
+    var ism = {
+      'source': $('#addOrUpdateIsm fieldset input#inputSource').val(),
+      'number': $('#addOrUpdateIsm fieldset input#inputNumber').val(),
+      'tags': $('#addOrUpdateIsm fieldset input#inputTags').val(),
+      'quote': $('#addOrUpdateIsm fieldset textarea#inputQuote').val(),
+      'comments': $('#addOrUpdateIsm fieldset textarea#inputComments').val(),
+    }
+    var url = '/isms/addorupdateism/';
+    var type = 'POST';
+    if ($(this).attr('value')) {
+      url += $(this).attr('value')
+      type = 'PUT';
+    }
+    console.log(url)
+    $.ajax({
+      type: type,
+      data: ism,
+      url: url,
+      dataType: 'JSON'
+    }).done(function( response ) {
+      if (response.msg === '') {
+        $('#addOrUpdateIsm fieldset input').val('');
+        $('#addOrUpdateIsm fieldset textarea').val('');
+        $('#addOrUpdateIsm fieldset button#btnAddOrUpdateIsm').val('');
+        populateTable();
+      } else {
+        alert('Error: ' + response.msg);
+      }
     });
-
-    if(errorCount === 0) {
-        var ism = {
-            'source': $('#addOrUpdateIsm fieldset input#inputSource').val(),
-            'number': $('#addOrUpdateIsm fieldset input#inputNumber').val(),
-            'tags': $('#addOrUpdateIsm fieldset input#inputTags').val(),
-            'quote': $('#addOrUpdateIsm fieldset textarea#inputQuote').val(),
-            'comments': $('#addOrUpdateIsm fieldset textarea#inputComments').val(),
-        }
-        var url = '/isms/addorupdateism/';
-        var type = 'POST';
-        if ($(this).attr('value')) {
-          url += $(this).attr('value')
-          type = 'PUT';
-        }
-        console.log(url)
-        $.ajax({
-            type: type,
-            data: ism,
-            url: url,
-            dataType: 'JSON'
-        }).done(function( response ) {
-            if (response.msg === '') {
-                $('#addOrUpdateIsm fieldset input').val('');
-                $('#addOrUpdateIsm fieldset textarea').val('');
-                $('#addOrUpdateIsm fieldset button#btnAddOrUpdateIsm').val('');
-                populateTable();
-            } else {
-                alert('Error: ' + response.msg);
-            }
-        });
-    }
-    else {
-        alert('Please fill in all fields');
-        console.log('exiting clearIsm with return false');
-        return false;
-    }
-    hideAddOrUpdateForm();
-    console.log('exiting addOrUpdateIsm');
+  }
+  else {
+    alert('Please fill in all fields');
+    console.log('exiting clearIsm with return false');
+    return false;
+  }
+  hideAddOrUpdateForm();
+  console.log('exiting addOrUpdateIsm');
 };
 
 // Add or Update Ism
 function clearIsm(event) {
-    event.preventDefault();
-    console.log('clear ism clicked!');
-    clearTheFields();
-    console.log('exiting clearIsm');
+  event.preventDefault();
+  console.log('clear ism clicked!');
+  clearTheFields();
+  console.log('exiting clearIsm');
 };
 
 
 // Delete Ism
 function deleteIsm(event) {
-    event.preventDefault();
-    console.log('delete ism clicked!');
+  event.preventDefault();
+  console.log('delete ism clicked!');
 
-    var confirmation = confirm('Are you sure you want to delete this ism?');
-    ismId = $(this).attr('rel');
-    if (confirmation === true) {
-        $.ajax({
-            type: 'DELETE',
-            url: '/isms/deleteism/' + ismId
-        }).done(function( response ) {
-            if (response.msg === '') {
-            } else {
-                alert('Error: ' + response.msg);
-            }
-            populateTable();
-            // clear the update fields if the id matches
-            if ($('#addOrUpdateIsm fieldset button#btnAddOrUpdateIsm').val() === ismId) {
-                $('#addOrUpdateIsm fieldset input').val('');
-            }
-        });
-    }
-    else {
-        console.log('exiting deleteIsm with return false');
-        return false;
-    }
-    console.log('exiting deleteIsm');
+  var confirmation = confirm('Are you sure you want to delete this ism?');
+  ismId = $(this).attr('rel');
+  if (confirmation === true) {
+    $.ajax({
+      type: 'DELETE',
+      url: '/isms/deleteism/' + ismId
+    }).done(function( response ) {
+      if (response.msg === '') {
+      } else {
+        alert('Error: ' + response.msg);
+      }
+      populateTable();
+      // clear the update fields if the id matches
+      if ($('#addOrUpdateIsm fieldset button#btnAddOrUpdateIsm').val() === ismId) {
+        $('#addOrUpdateIsm fieldset input').val('');
+      }
+    });
+  }
+  else {
+    console.log('exiting deleteIsm with return false');
+    return false;
+  }
+  console.log('exiting deleteIsm');
 };
 
 function populateIsmFields(event) {
-    event.preventDefault();
-    console.log('populatefieldsclicked!');
-    setUpdateIsmFormElementText();
+  event.preventDefault();
+  console.log('populatefieldsclicked!');
+  setUpdateIsmFormElementText();
 
-    // Retrieve ismname from link rel attribute
-    var thisIsmId = $(this).attr('rel');
+  // Retrieve ismname from link rel attribute
+  var thisIsmId = $(this).attr('rel');
 
-    console.log('id is ' + thisIsmId);
+  console.log('id is ' + thisIsmId);
 
-    // Get Index of object based on id value
-    var arrayPosition = ismListData.map(function(arrayItem) {
-        return arrayItem._id;
-    }).indexOf(thisIsmId);
+  // Get Index of object based on id value
+  var arrayPosition = ismListData.map(function(arrayItem) {
+      return arrayItem._id;
+  }).indexOf(thisIsmId);
 
-    // Get our Ism Object
-    var thisIsmObject = ismListData[arrayPosition];
+  // Get our Ism Object
+  var thisIsmObject = ismListData[arrayPosition];
 
-    //Populate Update Fields
-    console.log('the ism is ' + JSON.stringify(thisIsmObject));
+  //Populate Update Fields
+  console.log('the ism is ' + JSON.stringify(thisIsmObject));
 
-    // fill the ism to update field with the ismname
-    $('#ismBeingUpdated').text(thisIsmObject.ismname);
+  // fill the ism to update field with the ismname
+  $('#ismBeingUpdated').text(thisIsmObject.ismname);
 
-    // Inject the current value into the update field
-    $('#addOrUpdateIsm fieldset input#inputSource').val(thisIsmObject.source);
-    $('#addOrUpdateIsm fieldset input#inputNumber').val(thisIsmObject.number);
-    $('#addOrUpdateIsm fieldset input#inputTags').val(thisIsmObject.tags);
-    $('#addOrUpdateIsm fieldset textarea#inputQuote').val(thisIsmObject.quote);
-    $('#addOrUpdateIsm fieldset textarea#inputComments').val(thisIsmObject.comments);
-    $('#addOrUpdateIsm fieldset button#btnAddOrUpdateIsm').val(thisIsmObject._id);
+  // Inject the current value into the update field
+  $('#addOrUpdateIsm fieldset input#inputSource').val(thisIsmObject.source);
+  $('#addOrUpdateIsm fieldset input#inputNumber').val(thisIsmObject.number);
+  $('#addOrUpdateIsm fieldset input#inputTags').val(thisIsmObject.tags);
+  $('#addOrUpdateIsm fieldset textarea#inputQuote').val(thisIsmObject.quote);
+  $('#addOrUpdateIsm fieldset textarea#inputComments').val(thisIsmObject.comments);
+  $('#addOrUpdateIsm fieldset button#btnAddOrUpdateIsm').val(thisIsmObject._id);
 
-    console.log('exiting populateIsmFields');
+  console.log('exiting populateIsmFields');
 }
--- routes/isms.js
+++ routes/isms.js
@@ -5,52 +5,52 @@ var router = express.Router();
  * GET ismlist.
  */
 router.get('/ismlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('ismlist');
-    collection.find({},{},function(e,docs){
-        res.json(docs);
-    });
+  var db = req.db;
+  var collection = db.get('ismlist');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
 });
 
 /*
  * PUT to updateism.
  */
 router.put('/addorupdateism/:id', function(req, res) {
-    var db = req.db;
-    var collection = db.get('ismlist');
-    var ismToUpdate = req.params.id;
-    collection.update({ '_id' : ismToUpdate }, req.body, function(err) {
-        res.send(
-            (err === null) ? { msg: '' } : { msg:'error: ' + err }
-        );
-    });
+  var db = req.db;
+  var collection = db.get('ismlist');
+  var ismToUpdate = req.params.id;
+  collection.update({ '_id' : ismToUpdate }, req.body, function(err) {
+    res.send(
+      (err === null) ? { msg: '' } : { msg:'error: ' + err }
+    );
+  });
 });
 
 /*
  * POST to addism.
  */
 router.post('/addorupdateism', function(req, res) {
-    var db = req.db;
-    var collection = db.get('ismlist');
-    collection.insert(req.body, function(err, result){
-        res.send(
-            (err === null) ? { msg: '' } : { msg: err }
-        );
-    });
+  var db = req.db;
+  var collection = db.get('ismlist');
+  collection.insert(req.body, function(err, result){
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
 });
 
 /*
  * DELETE to deleteism.
  */
 router.delete('/deleteism/:id', function(req, res) {
-    var db = req.db;
-    var collection = db.get('ismlist');
-    var ismToDelete = req.params.id;
-    collection.remove({ '_id' : ismToDelete }, function(err) {
-        res.send(
-            (err === null) ? { msg: '' } : { msg:'error: ' + err }
-        );
-    });
+  var db = req.db;
+  var collection = db.get('ismlist');
+  var ismToDelete = req.params.id;
+  collection.remove({ '_id' : ismToDelete }, function(err) {
+    res.send(
+      (err === null) ? { msg: '' } : { msg:'error: ' + err }
+    );
+  });
 });
 
 module.exports = router;

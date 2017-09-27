6a480d9a82d6974ea4e1156af67eee2c4b5265b7
Matthew Odle <matt.odle@tdstelecom.com>
2017-09-26 22:40:29 -0500

    move auth management functions to auth.js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -119,18 +119,6 @@ $(document).ready(function() {
 
 // Functions =============================================================
 
-function logUserOut() {
-  deleteCookie('username');
-  clearIsmDivs();
-  clearTagCloud();
-  clearSourceCloud();
-  console.log('user is logged out');
-  hideButton('logout');
-  showButton('login');
-  hideModal(formModal);
-  showModal(loggedOutModal);
-  hideModalAfterAWhile(loggedOutModal);
-}
 
 function hideButton(buttonClass) {
   $('#' + buttonClass).hide();
@@ -163,73 +151,6 @@ function clearSourceCloud() {
 
 function clearIsmDivs() {
   setIsmsList('');
-}
-
-// Login event
-function logUserIn(event) {
-  event.preventDefault();
-  console.log('login button clicked!');
-
-  // Super basic validation - increase errorCount variable if any fields are blank
-  var errorCount = 0;
-  $('#loginForm input').each(function(index, val) {
-    console.log(this)
-    if ($(this).val() === '') {
-      errorCount++;
-    }
-  });
-
-  if(errorCount === 0) {
-    var user = {
-      'username': $('#loginForm fieldset input#inputUsername').val(),
-      'password': $('#loginForm fieldset input#inputPassword').val(),
-    }
-    var url = '/login/';
-    var type = 'POST';
-    $.ajax({
-      type: type,
-      data: user,
-      url: url,
-      dataType: 'JSON'
-    }).done(function( response ) {
-      if (response.msg === '') {
-        setCookie("username", user.username, 365);
-      } else {
-        alert('Error: ' + response.msg);
-      }
-      hideModal(loginModal);
-      generateContent('');
-      showButton('logout');
-      hideButton('login');
-    });
-  }
-  else {
-    alert('Please fill in all fields');
-    console.log('exiting login with return false');
-    return false;
-  }
-  console.log('exiting logUserIn');
-};
-
-function checkLoggedIn() {
-  var user = getCookie("username");
-  if (user != "") {
-    console.log("user is logged in");
-    return true;
-  }
-  return false;
-}
-
-function promptUserToLogin() {
-  showModal(loginModal);
-  $('#inputUsername').focus();
-}
-
-function handleLogin() {
-  if (!checkLoggedIn()) {
-    console.log("user is not logged in");
-    promptUserToLogin();
-  }
 }
 
 function openNewIsmForm() {

278a95c1066bfdd7dbb28ed6462f3a0ed4c854a9
Matthew Odle <matt.odle@tdstelecom.com>
2017-09-27 22:27:22 -0500

    clean up

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -41,25 +41,25 @@ function logUserIn(event) {
   // Super basic validation - increase errorCount variable if any fields are blank
   var errorCount = 0;
   $('#loginForm input').each(function(index, val) {
-      console.log(this)
-      if ($(this).val() === '') {
+    console.log(this)
+    if ($(this).val() === '') {
       errorCount++;
-      }
+    }
   });
 
   if(errorCount === 0) {
-      var user = {
+    var user = {
       'username': $('#loginForm fieldset input#inputUsername').val(),
       'password': $('#loginForm fieldset input#inputPassword').val(),
-      }
-      var url = '/login/';
-      var type = 'POST';
-      $.ajax({
+    }
+    var url = '/login/';
+    var type = 'POST';
+    $.ajax({
       type: type,
       data: user,
       url: url,
       dataType: 'JSON'
-      }).done(function( response ) {
+    }).done(function( response ) {
       if (response.msg === '') {
           setCookie("username", user.username, 365);
       } else {
@@ -69,12 +69,12 @@ function logUserIn(event) {
       generateContent('');
       showButton('logout');
       hideButton('login');
-      });
+    });
   }
   else {
-      alert('Please fill in all fields');
-      console.log('exiting login with return false');
-      return false;
+    alert('Please fill in all fields');
+    console.log('exiting login with return false');
+    return false;
   }
   console.log('exiting logUserIn');
 };

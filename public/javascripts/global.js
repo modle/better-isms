bc71905faed9869435fe06657d450f4a1569741d
Matthew Odle <matt.odle@tdstelecom.com>
2017-10-06 21:23:22 -0500

    hide modals via css property instead of jquery on load

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,55 +1,53 @@
 $(document).ready(function() {
-
   // Click Entry Point Definitions =============================================================
 
-  $('#login').on('click', promptUserToLogin);
-  $('#btnSubmitLogin').on('click', logUserIn);
-  $('#logout').on('click', logUserOut);
+  $("#login").on("click", promptUserToLogin);
+  $("#btnSubmitLogin").on("click", logUserIn);
+  $("#logout").on("click", logUserOut);
 
-  $('#upsertSource').on('click', openUpsertSourceForm);
-  $('#upsertSource2').on('click', openUpsertSourceForm);
-  $('#btnSubmitUpsertSource').on('click', upsertSource);
-  $('#btnEditSource').on('click', openUpsertSourceForm);
+  $("#upsertSource").on("click", openUpsertSourceForm);
+  $("#upsertSource2").on("click", openUpsertSourceForm);
+  $("#btnSubmitUpsertSource").on("click", upsertSource);
+  $("#btnEditSource").on("click", openUpsertSourceForm);
 
   // Show new ism form on source select
-  $('#sourceListDiv').on('click', 'a.linksource', openNewIsmForm);
-  $('#newIsm').on('click', promptSourceSelection);
-  $('#btnShowBulkAddIsm').on('click', openBulkAddIsmForm);
-  $('#btnSubmitBulkAddIsm').on('click', bulkUpsertIsms);
-  $('#btnUpsertIsm').on('click', upsertIsm);
-  $('#btnClearIsm').on('click', clearIsm);
+  $("#sourceListDiv").on("click", "a.linksource", openNewIsmForm);
+  $("#newIsm").on("click", promptSourceSelection);
+  $("#btnShowBulkAddIsm").on("click", openBulkAddIsmForm);
+  $("#btnSubmitBulkAddIsm").on("click", bulkUpsertIsms);
+  $("#btnUpsertIsm").on("click", upsertIsm);
+  $("#btnClearIsm").on("click", clearIsm);
 
-  $('.hideModals').on('click', hideAllModals);
-  $('#clearFilter').on('click', generateContent);
+  $(".hideModals").on("click", hideAllModals);
+  $("#clearFilter").on("click", generateContent);
 
-  $('#ismList isms').on('click', 'a.linkupdateism', populateIsmFields);
-  $('#ismList isms').on('click', 'a.linkdeleteism', deleteIsm);
+  $("#ismList isms").on("click", "a.linkupdateism", populateIsmFields);
+  $("#ismList isms").on("click", "a.linkdeleteism", deleteIsm);
 
-  $('#tagCloud').on('click', 'a.linktagfilter', generateContent);
-  $('#sourceCloud').on('click', 'a.linksourcefilter', generateContent);
-
+  $("#tagCloud").on("click", "a.linktagfilter", generateContent);
+  $("#sourceCloud").on("click", "a.linksourcefilter", generateContent);
 
   // Key Events =============================================================
-  $('#upsertIsmForm').keyup(function (event) {
+  $("#upsertIsmForm").keyup(function(event) {
     // enter or ctrl+s
     if (event.ctrlKey && event.keyCode == 83) {
-      $('#btnUpsertIsm').click();
+      $("#btnUpsertIsm").click();
       event.preventDefault();
     }
   });
 
   $(window).keydown(function(event) {
     // ctrl+s
-    if(event.ctrlKey && event.keyCode == 83) {
+    if (event.ctrlKey && event.keyCode == 83) {
       event.preventDefault();
     }
     // ctrl+i
-    if(event.ctrlKey && event.keyCode == 73) {
-      $('#newIsm').click();
+    if (event.ctrlKey && event.keyCode == 73) {
+      $("#newIsm").click();
       event.preventDefault();
     }
     // esc
-    if(event.keyCode == 27) {
+    if (event.keyCode == 27) {
       for (var modal of modals) {
         hideModal(modal);
       }
@@ -57,24 +55,29 @@ $(document).ready(function() {
     }
   });
 
-  $('#btnClearIsm').on('keydown', function (evt) {
-    if(evt.keyCode === 9) { // Tab pressed
+  $("#btnClearIsm").on("keydown", function(evt) {
+    if (evt.keyCode === 9) {
+      // Tab pressed
       evt.preventDefault();
-      $('#inputNumber').focus();
+      $("#inputNumber").focus();
     }
   });
 
-  $('input,select').keydown(function(event) {
-    var key = event.charCode ? event.charCode : event.keyCode ? event.keyCode : 0;
+  $("input,select").keydown(function(event) {
+    var key = event.charCode
+      ? event.charCode
+      : event.keyCode ? event.keyCode : 0;
     if (key == 13) {
       event.preventDefault();
-      var inputs = $('#upsertIsmForm').find(':input:visible');
+      var inputs = $("#upsertIsmForm").find(":input:visible");
       var nextinput = 0;
-      if (inputs.index(this) < (inputs.length - 1)) {
+      if (inputs.index(this) < inputs.length - 1) {
         nextinput = inputs.index(this) + 1;
       }
       if (inputs.length == 1) {
-        $(this).blur().focus();
+        $(this)
+          .blur()
+          .focus();
       } else {
         inputs.eq(nextinput).focus();
       }
@@ -82,32 +85,28 @@ $(document).ready(function() {
   });
 
   // get the modals
-  modals = document.getElementsByClassName('modal');
-
-  // close the modals on initial page load
-  hideAllModals();
+  modals = document.getElementsByClassName("modal");
 
   // generate the isms on initial page load if user is logged in
-  hideButton('logout');
-  hideButton('login');
+  hideButton("logout");
+  hideButton("login");
   if (checkLoggedIn()) {
-    generateContent('');
-    showButton('logout');
+    generateContent("");
+    showButton("logout");
   } else {
     console.log("user is not logged in");
     promptUserToLogin();
   }
-
 });
 
 // Functions =============================================================
 
 function hideButton(buttonClass) {
-  $('#' + buttonClass).hide();
+  $("#" + buttonClass).hide();
 }
 
 function showButton(buttonClass) {
-  $('#' + buttonClass).show();
+  $("#" + buttonClass).show();
 }
 
 function promptSourceSelection() {
@@ -117,20 +116,22 @@ function promptSourceSelection() {
 function openUpsertSourceForm(event) {
   handleLogin();
   showModal(upsertSourceModal);
-  var sourceId = $(this).attr('value');
+  var sourceId = $(this).attr("value");
   console.log(sourceId);
-  $('#sourceFormTitle').html('Add source')
-  $('#upsertSourceModal fieldset button#btnSubmitUpsertSource').val('');
-  $('#upsertSourceModal fieldset button#btnSubmitUpsertSource').html('Add');
-if (sourceId) {
+  $("#sourceFormTitle").html("Add source");
+  $("#upsertSourceModal fieldset button#btnSubmitUpsertSource").val("");
+  $("#upsertSourceModal fieldset button#btnSubmitUpsertSource").html("Add");
+  if (sourceId) {
     source = sourceCloudDict[sourceId];
-    $('#sourceFormTitle').html("Update source")
-    $('#upsertSourceModal fieldset input#inputTitle').val(source['title']);
-    $('#upsertSourceModal fieldset input#inputAuthor').val(source['author']);
-    $('#upsertSourceModal fieldset button#btnSubmitUpsertSource').val(sourceId);
-    $('#upsertSourceModal fieldset button#btnSubmitUpsertSource').html('Update');
+    $("#sourceFormTitle").html("Update source");
+    $("#upsertSourceModal fieldset input#inputTitle").val(source["title"]);
+    $("#upsertSourceModal fieldset input#inputAuthor").val(source["author"]);
+    $("#upsertSourceModal fieldset button#btnSubmitUpsertSource").val(sourceId);
+    $("#upsertSourceModal fieldset button#btnSubmitUpsertSource").html(
+      "Update"
+    );
   }
-  $('#inputTitle').focus();
+  $("#inputTitle").focus();
 }
 
 function hideAllModals() {
@@ -141,6 +142,6 @@ function hideAllModals() {
 }
 
 function clearAllForms() {
-  $('fieldset input').val('');
-  $('fieldset textarea').val('');
+  $("fieldset input").val("");
+  $("fieldset textarea").val("");
 }
